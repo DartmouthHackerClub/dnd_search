57f5a16660f2f3cb2b845744a18c63fe73f2ca17
Galen Pospisil <galen.m.pospisil@gmail.com>
2012-11-06 17:19:39 -0500

    added ability to filter for certain attributes, some basic styling

--- app.py
+++ app.py
@@ -11,55 +11,27 @@ def index():
 @app.route("/search")
 def search():
     err_msg = ""
+    raw_results = []
     results = []
+    attributes = ['cn','mail','dndAssignedNetid']
 
     query = request.args.get('query')
 
     if not query:
         err_msg = "Please specifiy a query"
     else:
-        results = lookup(query, None)
+        raw_results = lookup(query, None)
 
-    if results == None:
+    if raw_results == None:
         results = []
         err_msg = "LDAP query failed"
 
+    
+    for result in raw_results:
+        result = { attribute: result[attribute] for attribute in attributes }
+        results.append(result)
+
     return render_template('search.html', results=results, err_msg=err_msg)
-
-@app.route("/blitzlistr")
-def make_list():
-    err_msg = ""
-    qlist_txt = request.args.get('qlist_txt')
-
-    if qlist_txt:
-        qlist = re.split(',|;|\n', qlist_txt)
-    else:
-        qlist = []
-
-    conflicts = []
-    not_founds = []
-    results = []
-    for query in qlist:
-        r = lookup(query, ['mail'])
-        print r
-        if r is None:
-            err_msg = "LDAP query failed"
-            break
-        elif len(r) == 1:
-            results.append(r[0]['mail'][0])
-        elif len(r) == 0:
-            not_founds.append(query)
-        elif len(r) > 1:
-            mails = map(lambda x: x['mail'][0], r)
-            conflicts.append((query, mails))
-
-    to_string = "; ".join(results)
-    conflict_string = ", ".join(map(lambda c:c[0] , conflicts))
-    not_founds_string = ", ".join(not_founds)
-
-    return render_template('listr.html', to_string=to_string, \
-                            not_founds_string=not_founds_string, \
-                            conflict_string=conflict_string)
 
 if __name__ == "__main__":
     app.run(debug=True)
