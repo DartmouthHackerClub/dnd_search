90526f06215d0346b78de612486343f59cdb50cf
Zach Denton <zacharydenton@gmail.com>
2013-05-17 19:24:06 -0400

    Update to use the new DND API + custom Bootstrap.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, session, render_template, redirect
-from dnd_ldap import lookup
+from flask import Flask, request, render_template
+import requests
 import re
 
 app = Flask(__name__)
@@ -10,26 +10,13 @@ def index():
 
 @app.route("/search")
 def search():
-    err_msg = ""
-    raw_results = []
-    results = []
-    attributes = ['mail','dndAssignedNetid','cn','dndHinmanaddr','dndDeptclass','telephoneNumber']
-
     query = request.args.get('query')
-
-    if not query:
-        err_msg = "Please specify a query"
-    else:
-        results = lookup(query)
-
-    if results == None:
-        results = []
-        err_msg = "LDAP query failed"
+    results = requests.get("http://dnd.hackdartmouth.org/%s" % query).json()
 
     if request.is_xhr:
-        return render_template('search.html', results=results, err_msg=err_msg)
+        return render_template('search.html', results=results)
     else:
-        return render_template('index.html', results=results, err_msg=err_msg)
+        return render_template('index.html', results=results)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host='0.0.0.0', debug=False)
